3ebca902845aa0731a8ebd87eb0ef1042d0e4dd9
Joannis Orlandos <joannis@orlandos.nl>
2017-07-23 23:56:51 +0200

    Basic template

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/OpenKitten/Leopard.git", .revision("master")),
-        .package(url: "https://github.com/OpenKitten/Meow.git", .revision("codable")),
+        .package(url: "https://github.com/OpenKitten/Meow.git", .revision("framework")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WebsiteBackend",
-            dependencies: []),
+            dependencies: ["Meow", "Leopard"]),
     ]
 )
--- Sources/WebsiteBackend/main.swift
+++ Sources/WebsiteBackend/main.swift
@@ -1,1 +1,29 @@
-print("Hello, world!")
+import Meow
+import Leopard
+
+class BlogPost : Model, PathComponentExtracting {
+    var _id = ObjectId()
+    
+    var title: String
+    
+    static func extract(from string: String) throws -> BlogPost? {
+        let id = try ObjectId(string)
+        
+        return try BlogPost.findOne("_id" == id)
+    }
+    
+    init(titled title: String) {
+        self.title = title
+    }
+}
+
+let server = try SyncWebServer()
+
+server.get("posts", ":post") { request in
+    let post = try request.extract(BlogPost.self, from: "post")
+    
+    return post?.title ?? "None"
+}
+
+try server.start()
+
